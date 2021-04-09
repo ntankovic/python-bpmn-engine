6da928c44f6d5406bfbcfcd7d9e94e343c92c493
Nikola Tanković <nikola.tankovic@gmail.com>
2021-04-09 18:54:32 +0200

    Parallel execution

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pprint import pprint
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from copy import deepcopy
+from functools import partial
 
 
 model_tree = ET.parse("models/model_01.bpmn")
@@ -45,7 +46,10 @@ def check_conditions(state, conditions):
     print("DONE: Result is", ok)
 
 
-async def process(pending, elements, variables, flow):
+async def process(id, pending, elements, variables, flow):
+
+    prefix = f"\t[{id}]"
+    log = partial(print, prefix)
 
     pending = deepcopy(pending)
     elements = deepcopy(elements)
@@ -60,13 +64,13 @@ async def process(pending, elements, variables, flow):
             break
 
         if isinstance(current, Task):
-            print("\tDOING:", current)
+            log("DOING:", current)
 
         default = current.default if isinstance(current, ExclusiveGateway) else None
 
         can_continue = current.run()
         if not can_continue:
-            print("\t\t- waiting for all processes in gate.")
+            log("\t- waiting for all processes in gate.")
 
         if can_continue:
             next_tasks = []
@@ -83,20 +87,33 @@ async def process(pending, elements, variables, flow):
                         next_tasks.append(elements[sequence.target])
 
                 if not next_tasks and default_fallback:
-                    print("\t\t- going down default path...")
+                    log("\t- going down default path...")
                     next_tasks.append(default_fallback)
 
             pending += next_tasks
+    log("DONE")
 
 
-p1 = process(pending, elements, {"a": 1}, flow)
-p2 = process(pending, elements, {"a": 2}, flow)
+p1 = process("1", pending, elements, {"a": 1}, flow)
+p2 = process("2", pending, elements, {"a": 2}, flow)
 
 run = [p1, p2]
 
-for i, p in enumerate(run):
-    print(f"Running process {i+1}\n-----------------")
-    asyncio.run(p)
-    print()
 
-print("DONE!")
+def run_serial():
+    for i, p in enumerate(run):
+        print(f"Running process {i+1}\n-----------------")
+        asyncio.run(p)
+        print()
+
+
+def run_parallel():
+    async def parallel():
+        await asyncio.gather(p1, p2)
+
+    print(f"Running processes\n-----------------")
+    asyncio.run(parallel())
+
+
+# run_parallel()
+run_serial()
