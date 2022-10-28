bd9be683aa5c3f061dd4e0d1aa8b11f1a9485239
SempaiEcchi <minecraftm665@gmail.com>
2022-10-29 00:34:44 +0200

    fix query exception

--- db_connector.py
+++ db_connector.py
@@ -54,11 +54,12 @@ def add_event(
 
 @db_session
 def add_running_instance(instance_id, ran_as_subprocess=False, state="initialized"):
-    RunningInstance(instance_id=instance_id ,state=state, ran_as_subprocess=ran_as_subprocess)
+    RunningInstance(instance_id=instance_id, state=state, ran_as_subprocess=ran_as_subprocess)
+
 
 @db_session
 def change_instance_state(instance_id, state="initialized"):
-    RunningInstance[instance_id].state=state
+    RunningInstance[instance_id].state = state
 
 
 @db_session
@@ -68,12 +69,16 @@ def finish_running_instance(instance):
 
 
 @db_session
-def get_instances_log(state="running"):
+def get_instances_log(state=None):
     log = []
-    state_query = lambda ri: ri.state == state
-    if state is None:
-        state_query = None
-    running_instances = RunningInstance.select(state_query)[:]
+    running_instances = None
+
+    if state is not None:
+        state_query = lambda ri: ri.state == state
+        running_instances = RunningInstance.select(state_query)[:]
+    else:
+        running_instances = RunningInstance.select()[:]
+
     for instance in running_instances:
         instance_dict = {}
         instance_dict[instance.instance_id] = {}
@@ -103,8 +108,8 @@ def get_instance(id):
     instance_dict = {}
 
     db_instance: RunningInstance = RunningInstance.get(instance_id=id)
-    instance_dict["subprocess"]=db_instance.ran_as_subprocess
-    instance_dict["instance.state"]=db_instance.state
+    instance_dict["subprocess"] = db_instance.ran_as_subprocess
+    instance_dict["instance.state"] = db_instance.state
     events = Event.select(lambda e: e.instance_id == id).order_by(
         Event.timestamp
     )[:]
